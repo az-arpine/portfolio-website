3164a13e5e4d92bd5f734584a968abf0afeda09d
az-arpine <arpineazatyan@gmail.com>
2025-08-16 22:55:53 -0400

    integrated jQuery
    Added hamburger menu for mobile
    Changed colors
    Polished css

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,44 @@
 alert("Hello");
 console.log("Hello, welcome to the portfolio site!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+
+$(document).ready(function(){
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 600); // 600ms = smooth speed
+        }
+      }
+    });
+});
+
+$('html, body').animate({
+  scrollTop: target.offset().top - 80 // adjust "80" to your header height
+}, 600);
