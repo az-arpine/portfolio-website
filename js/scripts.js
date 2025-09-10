e35f74b158008ba502d68ebf1766a3b7298f1262
az-arpine <arpineazatyan@gmail.com>
2025-09-10 00:25:03 -0400

    Added video animations for musematch and saveup

--- js/scripts.js
+++ js/scripts.js
@@ -35,14 +35,6 @@ $(document).ready(function () {
       const coll = document.querySelector(".collapsible");
       const content = document.querySelector(".content");
 
-      coll.addEventListener("click", function () {
-        this.classList.toggle("active");
-        if (content.style.maxHeight) {
-          content.style.maxHeight = null;
-        } else {
-          content.style.maxHeight = content.scrollHeight + "px";
-        }
-      });
 
     });
 });
